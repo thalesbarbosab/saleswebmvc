a4a52f1b4322ec4b356c23d900a0964e1d86b07c
Thales Barbosa <thales@dreamsti.com.br>
2020-04-13 18:23:38 -0300

    Projeto ASP

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,37 @@
+﻿using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+        public void Insert (Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj=>obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+    }
+}
